721c4e128162eb541fd566ecbf9e96f7b3655067
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-11-30 18:43:56 +0100

    callback hell example

---
+++ async/callback.js
@@ -0,0 +1,64 @@
+'use strict';
+
+// Synchronous callback
+function printImmediately(print) {
+  print();
+}
+
+printImmediately(() => console.log('hello'));
+
+// Asynchronous callback
+function printWithDelay(print, timeout) {
+  setTimeout(print, timeout);
+}
+
+printWithDelay(() => console.log('async callback'), 2000);
+
+// Callback Hell example
+class UserStorage {
+  loginUser(id, password, onSuccess, onError) {
+    setTimeout(() => {
+      if (
+        (id === 'ellie' && password === 'dream') ||
+        (id === 'coder' && password === 'academy')
+      ) {
+        onSuccess(id);
+      } else {
+        onError(new Error('not found'));
+      }
+    }, 2000);
+  }
+
+  getRoles(user, onSuccess, onError) {
+    setTimeout(() => {
+      if (user === 'ellie') {
+        onSuccess({ name: 'ellie', role: 'admin' });
+      } else {
+        onError(new Error('not access'));
+      }
+    });
+  }
+}
+
+const userStorage = new UserStorage();
+const id = prompt('Enter your id');
+const password = prompt('Enter your password');
+
+userStorage.loginUser(
+  id,
+  password,
+  user => {
+    userStorage.getRoles(
+      user,
+      userWithRole => {
+        alert(
+          `Hello, ${userWithRole.name}, you have ${userWithRole.role} role`
+        );
+      },
+      error => {}
+    );
+  },
+  error => {
+    console.log(error);
+  }
+);
