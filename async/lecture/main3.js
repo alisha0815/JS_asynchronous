a81bd3fd4503fe141a1e0ca2fa3c0b1c7375e2b2
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-12-05 23:55:08 +0100

    async for rednering country data

---
+++ async/lecture/main3.js
@@ -0,0 +1,29 @@
+'use strict';
+// render country data
+const renderCountry = function (data, className = '') {
+  const html = `
+    <article class="country ${className}">
+      <img class="country__img" src="${data.flag}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>POP ${(
+          +data.population / 1000000
+        ).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      </div>
+    </article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+//async
+const whereAmI = async function (country) {
+  const res = await fetch(`https://restcountries.com/v2/name/${country}`);
+  const data = await res.json();
+  console.log(data[0]);
+};
+
+whereAmI('norway');
+console.log('First');
